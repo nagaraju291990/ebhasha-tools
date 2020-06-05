2390820c6462499847513d9c9b9762f840868c0b
Nagaraju <vpp.nagaraju1234@gmail.com>
2020-06-05 16:26:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# DEMO of websites
+
+Demo of websites 
+* Option to provide reference for the _source_ text from a **reference file or a URL**.  
+* Option to split and delete paras. Can insert special characters.
+
+
+### How to run:
+
+Load the [platform](http://localhost/ebhasha-tools) in browser.  
+
---
+++ index.php
@@ -0,0 +1,290 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="bootstrap-4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+		<title>Demo of websites</title>
+	</head>
+	<body>
+		<h1></h1>
+		<div class="app" data-finder-wrapper data-finder-scroll-offset="175">
+		<button class="finder-activator" data-finder-activator>
+	  Search
+	</button>
+		<div class="container container-fluid">
+			<div class="row content" data-finder-content>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Terms Validator</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Validator</h5>
+							<p class="card-text">Validating terms of format ((T1|T2|T3))</p>
+							<a href="http://183.82.119.160/term-validator/" target="_blank" class="btn btn-primary">Server1</a>
+							<a href="http://202.65.148.98/term-validator/" target="_blank" class="btn btn-primary">Server2</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Terms Utility</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Mark Terms/Easy Read</h5>
+							<p class="card-text">Mark terms of format ((T1|T2|T3))</p>
+							<a href="http://202.65.148.98/term-mark/" target="_blank" class="btn btn-primary">Server1</a>
+							<a href="http://183.82.119.160/term-mark/" target="_blank" class="btn btn-primary">Server2</a>
+							<a href="http://183.82.119.160/easy-read/" target="_blank" class="btn btn-primary">Easy Read</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Subtitle Validator</h5>
+						<div class="card-body">
+							<h5 class="card-title">Validator</h5>
+							<p class="card-text">Subtitle (srt) format validation</p>
+							<a href="http://183.82.119.160/subtitle-validator/" target="_blank" class="btn btn-primary">Server1</a>
+							<a href="http://202.65.148.98/subtitle-validator/" target="_blank" class="btn btn-primary">Server2</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Subtitle Utility</h5>
+						<div class="card-body">
+							<h5 class="card-title">Utility</h5>
+							<p class="card-text">Subtitle related tools</p>
+							<!--<a href="http://183.82.119.160/subtitle-utility/" target="_blank" class="btn btn-primary">Server1</a>-->
+							<a href="http://202.65.148.98/subtitle-utility/" target="_blank" class="btn btn-primary">Click to access</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Indic Transliteration</h5>
+						<div class="card-body">
+							<h5 class="card-title">Transliteration</h5>
+							<p class="card-text">English, Bengali, Kannada, MalayalaM, Hindi, Punjabi, Telugu, Tamil, Urdu</p>
+							<a href="http://183.82.119.160/indic-trans/" target="_blank" class="btn btn-primary">Click here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Transliteration</h5>
+						<div class="card-body">
+							<h5 class="card-title">Unicode/InPage transliteration</h5>
+							<p class="card-text">Hindi->Punjabi,Roman, Urdu->Hindi,Roman, Punjabi-Hindi, Roman, Telugu->Hindi</p>
+							<a href="http://183.82.119.160/indic-trans/" target="_blank" class="btn btn-primary">Click here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">PDF Search</h5>
+						<div class="card-body">
+							<h5 class="card-title">Search from BalBharathi/SCERT/ pdf text books</h5>
+							<p class="card-text">English -> Kannada, Hindi, Telugu, Urdu</p>
+							<a href="http://183.82.119.160/indic-trans/" target="_blank" class="btn btn-primary">Click here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Tokenizer</h5>
+						<div class="card-body">
+							<h5 class="card-title">File Viewer,  Tokenizer</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/tokenize-0.1/" target="_blank" class="btn btn-primary">Tokenize and Align Parallel</a>
+							<a href="http://183.82.119.160/tokenizer/" target="_blank" class="btn btn-primary"> Simple Tokenizer</a>
+							<a href="http://183.82.119.160/file-viewer5/" target="_blank" class="btn btn-primary">File Viewer</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">NMT Translation</h5>
+						<div class="card-body">
+							<h5 class="card-title">Neural Machine Translation</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://202.65.148.98/translate-onmt/" target="_blank" class="btn btn-primary">Open NMT</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Translation Work Bench</h5>
+						<div class="card-body">
+							<h5 class="card-title">Postediting tool</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://ebhashasetu.com/ebhashalsp/" target="_blank" class="btn btn-primary">Work Flow</a>
+							<a href="https://transzaar.com" target="_blank" class="btn btn-primary">Transzaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Transcription/SRT Work Bench</h5>
+						<div class="card-body">
+							<h5 class="card-title">Transcription/SRT editing tool</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/videozaar" target="_blank" class="btn btn-primary">VideoZaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Speech to Text</h5>
+						<div class="card-body">
+							<h5 class="card-title">Speech to Text/Audio to text verification</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/audiodata-0.8" target="_blank" class="btn btn-primary">AudioZaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Image to Text</h5>
+						<div class="card-body">
+							<h5 class="card-title">Image to text verification</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/imagedata-0.8" target="_blank" class="btn btn-primary">ImageZaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">PDF to Text</h5>
+						<div class="card-body">
+							<h5 class="card-title">PDF to text verification</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/pdfdata" target="_blank" class="btn btn-primary">PDFZaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Diff Tool</h5>
+						<div class="card-body">
+							<h5 class="card-title">Visusalize difference between two files</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://183.82.119.160/diff-0.3" target="_blank" class="btn btn-primary">PDFZaar</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Bleu/Parallel Dict</h5>
+						<div class="card-body">
+							<h5 class="card-title">Bleu Score calculation and Parallel Dict generation</h5>
+							<p class="card-text">Multiple languages</p>
+							<a href="http://202.65.148.98/bleu-calculator/" target="_blank" class="btn btn-primary">Bleu-calculator</a>
+							<a href="http://202.65.148.98/parallel-dict/" target="_blank" class="btn btn-primary">Paralle Dict</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Content here -->
+		</div>
+	</div>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+	        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	        crossorigin="anonymous"
+	></script>
+		<!--<script src="jQuery/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+		<script src="jQuery/jquery.highlight.js"></script>
+		<script src="jQuery/jquery.scrollto.js"></script>
+		<script src="jQuery/jquery.finder.js"></script>
+		<script src="bootstrap-4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+		<link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossorigin="anonymous"
+        />
+
+	</body>
+	<style>
+	.finder {
+	position: fixed;
+	top: -50px;
+	right: 0;
+	left: 0;
+	z-index: 999;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: -webkit-flex;
+	display: flex;
+	height: 50px;
+	background: #fff;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	border-bottom: 1px solid #e5e5e5;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	padding: 5px;
+	transition: top 0.15s ease-out;
+}
+
+.finder.active {
+	top: 0;
+}
+
+.finder-input {
+	flex-grow: 1;
+	width: 0;
+	height: 40px;
+	border: none;
+	padding: 8px;
+	outline: none !important;
+}
+
+.finder-input.not-found {
+	color: #e7353f;
+}
+
+.btn-finder {
+	width: 40px;
+	height: 40px;
+	text-align: center;
+	padding: 0;
+	color: #000;
+}
+
+.finder-input,
+.btn-finder:not(:last-child) {
+	margin-right: 5px;
+}
+
+.finder-count {
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: -webkit-flex;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	color: #888;
+	border-right: 1px solid #e5e5e5;
+	margin-right: 5px;
+	padding-right: 20px;
+	white-space: nowrap;
+}
+
+.btn-finder,
+.finder-count {
+	flex-shrink: 0;
+	flex-grow: 0;
+}
+
+.highlight {
+	background: #ff8;
+}
+
+.highlight.active {
+	background: #ffd71b;
+}	
+	</style>
+</html>
